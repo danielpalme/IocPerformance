473d068f1504a9c6972ea23fa0e5bf7c776551e1
danielpalme <daniel.palme@gmx.de>
2012-06-18 21:04:07 +0200

    Added Griffin.Container.

---
+++ IocPerformance/Adapters/GriffinContainerAdapter.cs
@@ -0,0 +1,30 @@
+﻿using Griffin.Container;
+
+namespace IocPerformance.Adapters
+{
+    public sealed class GriffinContainerAdapter : IContainerAdapter
+    {
+        private IParentContainer container;
+
+        public void Prepare()
+        {
+            var registrar = new ContainerRegistrar();
+            registrar.RegisterType<IInterface1, Implementation1>(Lifetime.Singleton);
+            registrar.RegisterType<IInterface2, Implementation2>(Lifetime.Transient);
+            registrar.RegisterType<ICombined, Combined>(Lifetime.Transient);
+
+            this.container = registrar.Build();
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            return this.container.Resolve<T>();
+        }
+
+        public void Dispose()
+        {
+            // Allow the container and everything it references to be disposed.
+            this.container = null;
+        }
+    }
+}
