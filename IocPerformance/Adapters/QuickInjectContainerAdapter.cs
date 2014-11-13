b26ec91a51c228cb820a3608ee2599c6426a643d
danielpalme <daniel.palme@gmx.de>
2014-11-13 22:44:35 +0100

    Added QuickInject

---
+++ IocPerformance/Adapters/QuickInjectContainerAdapter.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using IocPerformance.Classes.Child;
+using IocPerformance.Classes.Complex;
+using IocPerformance.Classes.Dummy;
+using IocPerformance.Classes.Properties;
+using IocPerformance.Classes.Standard;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using QuickInject;
+
+namespace IocPerformance.Adapters
+{
+    public sealed class QuickInjectContainerAdapter : ContainerAdapterBase
+    {
+        private QuickInjectContainer container;
+
+        public override string PackageName
+        {
+            get { return "QuickInject"; }
+        }
+
+        public override string Url
+        {
+            get { return "https://github.com/isabgol/QuickInject"; }
+        }
+
+        public override bool SupportsPropertyInjection
+        {
+            get { return true; }
+        }
+
+        public override bool SupportsChildContainer
+        {
+            get { return true; }
+        }
+
+        public override object Resolve(Type type)
+        {
+            return this.container.Resolve(type);
+        }
+
+        public override void Dispose()
+        {
+            // Allow the container and everything it references to be garbage collected.
+            this.container.Dispose();
+            this.container = null;
+        }
+
+        public override IChildContainerAdapter CreateChildContainerAdapter()
+        {
+            return new QuickInjectChildContainerAdapter(this.container.CreateChildContainer());
+        }
+
+        public override void Prepare()
+        {
+            this.container = new QuickInjectContainer();
+
+            this.RegisterDummies();
+            this.RegisterStandard();
+            this.RegisterComplex();
+            this.RegisterPropertyInjection();
+        }
+
+        private void RegisterDummies()
+        {
+            this.container.RegisterType<IDummyOne, DummyOne>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummyTwo, DummyTwo>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummyThree, DummyThree>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummyFour, DummyFour>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummyFive, DummyFive>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummySix, DummySix>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummySeven, DummySeven>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummyEight, DummyEight>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummyNine, DummyNine>(new TransientLifetimeManager());
+            this.container.RegisterType<IDummyTen, DummyTen>(new TransientLifetimeManager());
+        }
+
+        private void RegisterStandard()
+        {
+            this.container.RegisterType<ISingleton1, Singleton1>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<ISingleton2, Singleton2>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<ISingleton3, Singleton3>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<ITransient1, Transient1>(new TransientLifetimeManager());
+            this.container.RegisterType<ITransient2, Transient2>(new TransientLifetimeManager());
+            this.container.RegisterType<ITransient3, Transient3>(new TransientLifetimeManager());
+            this.container.RegisterType<ICombined1, Combined1>(new TransientLifetimeManager());
+            this.container.RegisterType<ICombined2, Combined2>(new TransientLifetimeManager());
+            this.container.RegisterType<ICombined3, Combined3>(new TransientLifetimeManager());
+        }
+
+        private void RegisterComplex()
+        {
+            this.container.RegisterType<IFirstService, FirstService>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<ISecondService, SecondService>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<IThirdService, ThirdService>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<ISubObjectOne, SubObjectOne>(new TransientLifetimeManager());
+            this.container.RegisterType<ISubObjectTwo, SubObjectTwo>(new TransientLifetimeManager());
+            this.container.RegisterType<ISubObjectThree, SubObjectThree>(new TransientLifetimeManager());
+            this.container.RegisterType<IComplex1, Complex1>(new TransientLifetimeManager());
+            this.container.RegisterType<IComplex2, Complex2>(new TransientLifetimeManager());
+            this.container.RegisterType<IComplex3, Complex3>(new TransientLifetimeManager());
+        }
+
+        private void RegisterPropertyInjection()
+        {
+            this.container.RegisterType<IServiceA, ServiceA>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<IServiceB, ServiceB>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<IServiceC, ServiceC>(new ContainerControlledLifetimeManager());
+            this.container.RegisterType<ISubObjectA, SubObjectA>(new TransientLifetimeManager());
+            this.container.RegisterType<ISubObjectB, SubObjectB>(new TransientLifetimeManager());
+            this.container.RegisterType<ISubObjectC, SubObjectC>(new TransientLifetimeManager());
+            this.container.RegisterType<IComplexPropertyObject1, ComplexPropertyObject1>(new TransientLifetimeManager());
+            this.container.RegisterType<IComplexPropertyObject2, ComplexPropertyObject2>(new TransientLifetimeManager());
+            this.container.RegisterType<IComplexPropertyObject3, ComplexPropertyObject3>(new TransientLifetimeManager());
+        }
+
+        private void RegisterInterceptor()
+        {
+            this.container.RegisterType<ICalculator1, Calculator1>(new TransientLifetimeManager())
+                 .Configure<Microsoft.Practices.Unity.InterceptionExtension.Interception>()
+                 .SetInterceptorFor<ICalculator1>(new InterfaceInterceptor());
+            this.container.RegisterType<ICalculator2, Calculator2>(new TransientLifetimeManager())
+                 .Configure<Microsoft.Practices.Unity.InterceptionExtension.Interception>()
+                 .SetInterceptorFor<ICalculator2>(new InterfaceInterceptor());
+            this.container.RegisterType<ICalculator3, Calculator3>(new TransientLifetimeManager())
+                 .Configure<Microsoft.Practices.Unity.InterceptionExtension.Interception>()
+                 .SetInterceptorFor<ICalculator3>(new InterfaceInterceptor());
+        }
+    }
+
+    public class QuickInjectChildContainerAdapter : IChildContainerAdapter
+    {
+        private IUnityContainer childContainer;
+
+        public QuickInjectChildContainerAdapter(IUnityContainer childContainer)
+        {
+            this.childContainer = childContainer;
+        }
+
+        public void Dispose()
+        {
+            this.childContainer.Dispose();
+        }
+
+        public void Prepare()
+        {
+            this.childContainer.RegisterType(typeof(ICombined1), typeof(ScopedCombined1));
+            this.childContainer.RegisterType(typeof(ICombined2), typeof(ScopedCombined2));
+            this.childContainer.RegisterType(typeof(ICombined3), typeof(ScopedCombined3));
+            this.childContainer.RegisterType(typeof(ITransient1), typeof(ScopedTransient));
+        }
+
+        public object Resolve(Type resolveType)
+        {
+            return this.childContainer.Resolve(resolveType);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -43,6 +43,7 @@ Results
 |**[Munq 3.1.6](http://munq.codeplex.com)**|111<br/>79|283<br/>192|751<br/>454|2389<br/>1482|
 |**[Ninject 3.2.2.0](http://ninject.org)**|7259<br/>4486|24276<br/>15296|69214<br/>39610|191033*<br/>117563|
 |**[Petite 0.3.2](https://github.com/andlju/Petite)**|6626<br/>4071|5546<br/>3373|6971<br/>4657|8246<br/>6118|
+|**[QuickInject 1.0.0.7](https://github.com/isabgol/QuickInject)**|171<br/>184|203<br/>189|224<br/>232|519<br/>594|
 |**[SimpleInjector 2.6.1](https://simpleinjector.org)**|63<br/>68|99<br/>84|121<br/>105|171<br/>129|
 |**[Spring.NET 1.3.2](http://www.springframework.net/)**|1046<br/>808|16566<br/>10128|45510<br/>29798|117230<br/>75645|
 |**[StructureMap 3.1.4.143](http://structuremap.net/structuremap)**|2531<br/>2831|2787<br/>2845|8181<br/>8274|19198<br/>20205|
@@ -77,6 +78,7 @@ Results
 |**[Munq 3.1.6](http://munq.codeplex.com)**|1899<br/>1169|<br/>|<br/>|<br/>|<br/>|<br/>|
 |**[Ninject 3.2.2.0](http://ninject.org)**|165177<br/>106569|67347<br/>42019|151450<br/>96672|53576<br/>31926|45724250*<br/>37677615*|36162<br/>22413|
 |**[Petite 0.3.2](https://github.com/andlju/Petite)**|6297<br/>3789|<br/>|<br/>|<br/>|<br/>|<br/>|
+|**[QuickInject 1.0.0.7](https://github.com/isabgol/QuickInject)**|163<br/>151|<br/>|<br/>|<br/>|1222506*<br/>931325*|<br/>|
 |**[SimpleInjector 2.6.1](https://simpleinjector.org)**|268<br/>194|102<br/>91|1214<br/>597|222<br/>169|<br/>|8573<br/>5065|
 |**[Spring.NET 1.3.2](http://www.springframework.net/)**|103584<br/>66296|<br/>|<br/>|<br/>|<br/>|77502<br/>43561|
 |**[StructureMap 3.1.4.143](http://structuremap.net/structuremap)**|18987<br/>19486|5485<br/>8342|17175<br/>18164|<br/>|3921913*<br/>2703535*|18162<br/>12085|
