cead9edcaf376e19699b68a875abff6e12a63b21
James Skimming <JSkimming@users.noreply.github.com>
2017-04-29 19:18:19 +0100

    Upgraded abioc to version 0.2.0

--- IocPerformance/Adapters/AbiocContainerAdapter.cs
+++ IocPerformance/Adapters/AbiocContainerAdapter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Reflection;
 using Abioc;
+using Abioc.Registration;
 using IocPerformance.Classes.Complex;
 using IocPerformance.Classes.Dummy;
 using IocPerformance.Classes.Standard;
@@ -8,8 +10,7 @@ namespace IocPerformance.Adapters
 {
     public sealed class AbiocContainerAdapter : ContainerAdapterBase
     {
-        private readonly DefaultContructionContext _contructionContext = new DefaultContructionContext();
-        private CompilationContext<DefaultContructionContext> _compilationContext;
+        private AbiocContainer _compilationContext;
 
         public override string PackageName => "abioc";
 
@@ -17,7 +18,7 @@ namespace IocPerformance.Adapters
 
         public override object Resolve(Type type)
         {
-            return _compilationContext.GetService(_contructionContext, type);
+            return _compilationContext.GetService(type);
         }
 
         public override void Dispose()
@@ -27,18 +28,18 @@ namespace IocPerformance.Adapters
 
         public override void PrepareBasic()
         {
-            var registrationContext = new RegistrationContext<DefaultContructionContext>();
+            var setup = new RegistrationSetup();
 
-            RegisterDummies(registrationContext);
-            RegisterStandard(registrationContext);
-            RegisterComplex(registrationContext);
+            RegisterDummies(setup);
+            RegisterStandard(setup);
+            RegisterComplex(setup);
 
-            _compilationContext = registrationContext.Compile(GetType().Assembly);
+            _compilationContext = setup.Construct(GetType().GetTypeInfo().Assembly);
         }
 
-        private static void RegisterDummies(RegistrationContext<DefaultContructionContext> registrationContext)
+        private static void RegisterDummies(RegistrationSetup setup)
         {
-            registrationContext
+            setup
                 .Register<IDummyOne, DummyOne>()
                 .Register<IDummyTwo, DummyTwo>()
                 .Register<IDummyThree, DummyThree>()
@@ -51,16 +52,16 @@ namespace IocPerformance.Adapters
                 .Register<IDummyTen, DummyTen>();
         }
 
-        private static void RegisterStandard(RegistrationContext<DefaultContructionContext> registrationContext)
+        private static void RegisterStandard(RegistrationSetup setup)
         {
             var singleton1 = new Singleton1();
             var singleton2 = new Singleton2();
             var singleton3 = new Singleton3();
 
-            registrationContext
-                .Register<ISingleton1>(c => singleton1)
-                .Register<ISingleton2>(c => singleton2)
-                .Register<ISingleton3>(c => singleton3)
+            setup
+                .RegisterFactory<ISingleton1>(() => singleton1)
+                .RegisterFactory<ISingleton2>(() => singleton2)
+                .RegisterFactory<ISingleton3>(() => singleton3)
                 .Register<ITransient1, Transient1>()
                 .Register<ITransient2, Transient2>()
                 .Register<ITransient3, Transient3>()
@@ -69,16 +70,16 @@ namespace IocPerformance.Adapters
                 .Register<ICombined3, Combined3>();
         }
 
-        private static void RegisterComplex(RegistrationContext<DefaultContructionContext> registrationContext)
+        private static void RegisterComplex(RegistrationSetup setup)
         {
             var firstService = new FirstService();
             var secondService = new SecondService();
             var thirdService = new ThirdService();
 
-            registrationContext
-                .Register<IFirstService>(c => firstService)
-                .Register<ISecondService>(c => secondService)
-                .Register<IThirdService>(c => thirdService)
+            setup
+                .RegisterFactory<IFirstService>(() => firstService)
+                .RegisterFactory<ISecondService>(() => secondService)
+                .RegisterFactory<IThirdService>(() => thirdService)
                 .Register<ISubObjectOne, SubObjectOne>()
                 .Register<ISubObjectTwo, SubObjectTwo>()
                 .Register<ISubObjectThree, SubObjectThree>()
