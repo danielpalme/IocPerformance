706b88d08d0eb0734b57aa832de6f1488020b3aa
Lam Le <lam281990@gmail.com>
2017-08-03 11:56:41 -0700

    Add JsonOutput

---
+++ IocPerformance/Output/JsonOutput.cs
@@ -0,0 +1,21 @@
+﻿using IocPerformance.Benchmarks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IocPerformance.Output
+{
+    public class JsonOutput : IOutput
+    {
+        public void Create(IEnumerable<IBenchmark> benchmarks, IEnumerable<BenchmarkResult> benchmarkResults)
+        {
+            if (!Directory.Exists("output"))
+            {
+                Directory.CreateDirectory("output");
+            }
+            var jsonResult = JsonConvert.SerializeObject(benchmarkResults, Formatting.Indented);
+
+            File.WriteAllText("result.json", jsonResult);
+        }
+    }
+}
